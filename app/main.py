83552150e33d8e12bd76005e742019dca4819776
DaniilNikonorov <daniilnikonorov@bk.ru>
2023-11-22 22:37:06 +0300

    second commit

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import uvicorn
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+''' 
+Для локального запуска
+if __name__ == "__main__":
+    uvicorn.run(app, port=9999)
+    
+'''
